b1a99e4ba5156fac5098e1e2f37b52f4fb6ef1bb
ranaro <robrownn@gmail.com>
2017-12-08 13:39:48 -0500

    Created a HomeIndex Output ViewModel to hold the message of the day and resatuarants and output them on the index view

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -3,22 +3,28 @@
 using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                              IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
             // Avoid using http request headers within a controller
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
 
             // The controller doesn't decide what the result is, just that it does the thing you're telling it to do
             return View(model);
