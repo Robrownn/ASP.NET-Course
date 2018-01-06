8cf24d5942a8edafa78e970c26206f7d9dff71e4
Robert Brown <robrownn@gmail.com>
2018-01-06 16:54:51 -0500

    Added a default gitignore. Finished the Entity Framework module. Created a DbContext for this app. Created db migrations. The app now uses a database rather than in-memory storage.

---
+++ OdeToFood/OdeToFood/Services/SQLRestaurantData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SQLRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDBContext _context;
+
+        public SQLRestaurantData(OdeToFoodDBContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+            // Save changes can be moved to a seperate method in order to do batch jobs
+            _context.SaveChanges();
+
+            // the new restaurant will always generate a new id
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            // If there is a large amount of records in the table, we probably want to use IQueryable
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
